a653ff9930615ca1c1d8a1ac1cb764804f3e5acf
syedomair <syedkhalidomair@gmail.com>
2015-08-23 15:26:16 +0500

    working API connect

--- src/SyedOmair/Bundle/AppBundle/Controller/CatalogController.php
+++ src/SyedOmair/Bundle/AppBundle/Controller/CatalogController.php
@@ -7,6 +7,8 @@ class CatalogController extends Controller
 {
     public function indexAction(){
 
+        $api = $this->get('client_service')->findCategories('1');
+        var_dump($api);
         return $this->render('AppBundle:Catalog:index.html.twig',
                 array( 
                 ));
---
+++ src/SyedOmair/Bundle/AppBundle/Services/ApiClientService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SyedOmair\Bundle\AppBundle\Services;
+
+use Guzzle\Http\Client;
+
+class ApiClientService
+{
+    protected $container;
+    protected $end_point;
+
+    public function __construct($container, $api_server_end_point)
+    {
+        $this->container = $container;
+        $this->end_point = $api_server_end_point;
+    }
+
+    public function findCategories($catalog_id)
+    {
+        $client = new Client($this->end_point);
+        $request = $client->get('api/categories/'.$catalog_id);
+        $response = $request->send();
+        $responseBody= $response->getBody();
+        $responseArray = json_decode($responseBody, true);
+        return $responseArray;
+    }
+
+    public function findProducts($product_id)
+    {
+        $client = new Client($this->end_point);
+        $request = $client->get('api/products/'.$product_id);
+        $response = $request->send();
+        $responseBody= $response->getBody();
+        $responseArray = json_decode($responseBody, true);
+        return $responseArray;
+    }
+
+}
